ad47f89515248547abcda652600e2aad1e615e54
srivathsansri <61596298+srivathsansri@users.noreply.github.com>
2020-08-18 01:42:53 +0530

    My First Website

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
+$(document).ready
+(
+    function()
+    {
+
+    $nav=$('.nav');
+    $toggleCollapse=$('.toggle-collapse');
+
+
+    /**click event on toggle */
+    $toggleCollapse.click
+    (
+        function()
+        {
+        $nav.toggleClass('collapse');
+        }
+    )
+//owl carousel for blog
+$('.owl-carousel').owlCarousel
+(
+    {
+
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:responsive
+    }
+);
+
+
+//click to scroll up
+$('.move-up span').click(function()
+{
+    $('html,body').animate({
+        scrollTop:0
+    },2000)
+})
+
+//animation on scroll
+
+AOS.init();
+});
